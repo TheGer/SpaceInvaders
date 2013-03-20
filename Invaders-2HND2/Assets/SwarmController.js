64baec752f18256639b4e3829f7f5052d5e581b2
Gerry Said <gerrysaid@gmail.com>
2013-03-20 10:39:55 +0100

    Lesson 4
    Updated swarm controller to programmatically generate aliens
    Created a function with two parameters to create rows and columns of
    aliens

--- Invaders-2HND2/Assets/SwarmController.js
+++ Invaders-2HND2/Assets/SwarmController.js
@@ -1,15 +1,59 @@
 #pragma strict
 //left margin in world coordinates
-var leftborder:float=-10.0;
+var leftborder:float=-12.0;
 
-var rightborder:float=8.8;
+var rightborder:float=6;
 
 var touchedrightwall:boolean=false;
 var touchedleftwall:boolean=false;
 
+//the instance of the alien I am going to generate.
+var alien:Rigidbody;
+
+
+//we need to move down in 11 steps.  We will wait 5 seconds before we move down.
+
+//a coroutine that is run in sequence in conjunction with the update function
+function moveDown()
+{
+	//move down by one step
+	transform.position.y--;
+}
+
+
+function createAliens(rows:int,cols:int)
+{
+	//for all the rows of aliens
+	for(var row=0;row<rows;row++)
+	{
+		//creates the aliens as required
+		for(var counter=0;counter<cols;counter++)
+		{
+			var tempAlien:Rigidbody;
+			//create instances of the alien in these positions
+			tempAlien = Instantiate(alien,Vector3(counter*2,transform.position.y+row,1),Quaternion.identity);
+			//the parent of the alien is the swarm
+			tempAlien.transform.parent = this.transform;
+		}
+	}
+	
+}
+
+
+
 
 function Start () {
-
+	//this loop will run 5 times
+	//create one row with five aliens
+	createAliens(2,5);
+	
+	for(var counter=0;counter<5;counter++)
+	{
+		//wait for five seconds
+		yield WaitForSeconds(5);
+		//move the aliens down
+		moveDown();
+	}
 }
 
 function Update () {
